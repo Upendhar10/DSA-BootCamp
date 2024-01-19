09382f3e7f433b28e79173adc04a60c3a534612e
Upendhar Nemmani <80301044+Upendhar10@users.noreply.github.com>
2024-01-19 14:49:41 +0530

    Create 05-Insertion-Sort-Algorithm.java

---
+++ 02-BASIC-SORTING/05-Insertion-Sort-Algorithm.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1] that are greater than key to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            // Insert the key at its correct position
+            array[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+        insertionSort(array);
+
+        System.out.println("Sorted array:");
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+    }
+}
